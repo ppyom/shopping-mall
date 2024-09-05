8a503fc3943dd4d3a500b2ffbfa7f564240c49f1
ppyom <ouob77@kakao.com>
2024-09-06 02:20:33 +0900

    feat: Filter 구현

--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -1,9 +1,33 @@
-const Filter = () => {
+import styles from './Filter.module.css';
+
+interface Props {
+  current: FilterValue | null;
+  update: (filter: FilterValue | null) => void;
+}
+
+const filterList = [
+  { title: '등록순', value: 'created' },
+  { title: '낮은 가격', value: 'price' },
+  { title: '높은 가격', value: '-price' },
+  { title: '높은 할인율', value: '-discount' },
+] as const;
+type FilterValue = (typeof filterList)[number]['value'];
+
+const Filter = ({ current, update }: Props) => {
   return (
-    <nav>
-      <button></button>
+    <nav className={styles.filter}>
+      {filterList.map((filter) => (
+        <button
+          key={`filter_${filter.value}`}
+          className={`${current === filter.value ? styles.active : ''}`}
+          onClick={() => update(current === filter.value ? null : filter.value)}
+        >
+          {filter.title}
+        </button>
+      ))}
     </nav>
   );
 };
 
 export default Filter;
+export type { FilterValue };
--- src/pages/ProductPage/ProductPage.tsx
+++ src/pages/ProductPage/ProductPage.tsx
@@ -1,12 +1,19 @@
+import { useState } from 'react';
 import PageLayout from '../../components/layouts/PageLayout/PageLayout.tsx';
-import Filter from '../../components/Filter/Filter.tsx';
+import Filter, { type FilterValue } from '../../components/Filter/Filter.tsx';
 import Products from '../../components/Products/Products.tsx';
 
 const ProductPage = () => {
+  const [filter, setFilter] = useState<FilterValue | null>(null);
+
+  const handleUpdateFilter = (filter: FilterValue | null) => {
+    setFilter(filter);
+  };
+
   return (
     <PageLayout>
       <h2>Shop The Latest</h2>
-      <Filter />
+      <Filter current={filter} update={handleUpdateFilter} />
       <Products products={[]} />
     </PageLayout>
   );
