a56ac02fc6c44ad650e2fc37dae7ca07b49d280a
ppyom <ouob77@kakao.com>
2024-09-09 13:55:53 +0900

    design: DetailSection 스타일 추가

--- src/components/layouts/DetailSection/DetailSection.tsx
+++ src/components/layouts/DetailSection/DetailSection.tsx
@@ -1,5 +1,7 @@
 import { useState } from 'react';
 import Counter from '../../Counter/Counter.tsx';
+import styles from './DetailSection.module.css';
+import type { Product } from '../../../types/product.ts';
 
 interface Props extends Product {}
 
@@ -13,30 +15,35 @@ const DetailSection = ({ img, title, price, discount }: Props) => {
   };
 
   return (
-    <section>
-      <h3>상품 상세 정보</h3>
-      <div>
+    <section className={styles.detailSection}>
+      <h3 hidden>상품 상세 정보</h3>
+      <div className={styles.imageCon}>
         <img src={`/images/${img}`} alt={title} />
       </div>
-      <div>
-        <p>{title}</p>
-        <p>
-          <span>{price.toLocaleString()}원</span>
+      <div className={styles.info}>
+        <p className={styles.title}>{title}</p>
+        <p className={styles.priceCon}>
+          <span className={`${discount !== 0 ? styles.cancel : ''}`}>
+            {price.toLocaleString()}원
+          </span>
           {discount !== 0 && (
             <>
-              <span>-{discount}%</span>
-              <span>
+              <span className={styles.discount}>-{discount}%</span>
+              <span className={styles.discounted}>
                 {((price * (100 - discount)) / 100).toLocaleString()}원
               </span>
             </>
           )}
+        </p>
+        <p className={styles.description}>
+          {/* TODO Data type에 description 추가 */}
         </p>
         <Counter
           count={count}
           increment={handleIncrement}
           decrement={handleDecrement}
         />
-        <button className="">장바구니에 추가</button>
+        <button className={styles.addBtn}>장바구니에 추가</button>
       </div>
     </section>
   );
